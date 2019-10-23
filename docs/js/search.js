20a9851204c201acd900597c9cf65a53a7806821
nak-244 <t09024656860@gmail.com>
2019-10-23 10:14:01 +0900

    up

--- docs/js/search.js
+++ docs/js/search.js
@@ -1,8 +1,11 @@
     $(function() {
       var href = "https://sigotora.jp/index.cfm";
-      $('#srh_ken_param,#sfw').on('change', function() {
+      $('#srh_ken_param,#sfw,.srh_jobtype_child_param').on('change', function() {
         var srh_ken_param = $('#srh_ken_param').val();
         var sfw = $('#sfw').val();
-        $('#test').prop('href', href + "?fuseaction=job.joblist&srh_jobtype_param=5&srh_ken_param=" + srh_ken_param + "&sfw=" + sfw);
+        var srh_jobtype_child_param = $('.srh_jobtype_child_param:checked').map(function() {
+                      return $(this).val();
+                    }).get();
+        $('#test').prop('href', href + "?fuseaction=job.joblist&srh_jobtype_param=5&srh_ken_param=" + srh_ken_param + "&srh_jobtype_child_param=" + srh_jobtype_child_param + "&sfw=" + sfw);
       });
     });
